d2c810baffeff272657ab40d7af7638cb5325845
N-buga <bugakova_nadya@mail.ru>
2016-02-27 11:21:15 +0300

    Fixed slave and hardware interrupt

--- src/create_idt.c
+++ src/create_idt.c
@@ -3,7 +3,9 @@
 #include "ioport.h"
 
 void outer_serial();
-void other_outer();
+void other_outer_master();
+void other_outer_slave();
+void hardware_outer();
 
 struct Descriptor ptr[32 + 16];
 
@@ -20,13 +22,18 @@ void create_idt() {
 
 //	printc(100);
 	uint64_t timer_handler = (uint64_t) &outer_serial;
-	uint64_t other_handler = (uint64_t) &other_outer;
+	uint64_t other_handler_master = (uint64_t) &other_outer_master;
+	uint64_t other_handler_slave = (uint64_t) &other_outer_slave;
+	uint64_t hardware_handler = (uint64_t) &hardware_outer;
 	set_descriptor(32, timer_handler); 
-	for (int i = 1; i < 16; i++) {
-		set_descriptor(32 + i, other_handler);
+	for (int i = 1; i < 8; i++) {
+		set_descriptor(32 + i, other_handler_master);
+	}
+	for (int i = 8; i < 16; i++) {
+		set_descriptor(32 + i, other_handler_slave);
 	}
 	for (int j = 0; j < 32; j++) {
-		set_descriptor(j, other_handler);
+		set_descriptor(j, hardware_handler);
 	} 
 	
 	return;		
--- src/other_inner_slave.c
+++ src/other_inner_slave.c
@@ -1,7 +1,8 @@
 #include "ioport.h"
 
-void other_inner() {
+void other_inner_slave() {
 	prints("Bue!");
 	out8(0x20, 0x20);
+	out8(0xA0, 0x20);
 }
 
